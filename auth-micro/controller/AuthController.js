7a899684829e96c39cc7f1e1982871730129cf8b
Amarathunga A.A.N.N <nilina610@gmail.com>
2026-01-13 23:33:38 +0530

    refactor: update database configuration, enhance user registration, and add bcrypt dependency

--- auth-micro/controller/AuthController.js
+++ auth-micro/controller/AuthController.js
@@ -1,8 +1,17 @@
+import bcrypt from "bcrypt"
+import prisma from "../config/db.config.js";
+
 class AuthController {
     static async register(req, res) {
-        const paylaod = req.body
+        const payload = req.body;
+        const salt = bcrypt.genSaltSync(10)
+        payload.password = bcrypt.hashSync(payload.password, salt);
 
-        return res.json({ paylaod })
+        const user = await prisma.user.create({
+            data: payload,
+        })
+
+        return res.json({ payload })
     }
 }
 
--- auth-micro/server.js
+++ auth-micro/server.js
@@ -5,11 +5,15 @@ const PORT = process.env.PORT || 5001
 
 //Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended }))
+app.use(express.urlencoded({ extended: false }))
 app.use(cors());
 
 app.get('/', (req, res) => {
     return res.json({ message: "yayy it's working" })
 })
+
+//Routes
+import Routes from "./routes/index.js"
+app.use(Routes);
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
